33dc0bf87346c95ebb24a891e691ede9f15ce7a0
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-04-25 22:12:33 -0400

    updates

--- scripts/default_scripts.js
+++ scripts/default_scripts.js
@@ -1,4 +1,34 @@
 //bxSlider used on Store page
-$(document).ready(function(){
-    $('.bxslider').bxSlider();
-  });
+//$(document).ready(function(){
+//    $('.bxslider').bxSlider();
+ // });
+
+//jQuery call to the accordion() method.
+//$(document).ready(function() {
+//  $("#projects-container").accordion({
+//      collapsible: true, 
+//      heightStyle: "content"
+//  });
+//});
+
+//JQuery call to the Tooltip() method
+//$(document).ready(function() {
+//  $(".review-form").tooltip({
+//  });
+//});
+
+setReviews = (e) => {
+  e.preventDefault();
+  var review = document.getElementById('review')
+  localStorage.setItem('review', review.value);
+  var reviews = document.getElementById('reviews');
+  var storedReview = localStorage.getItem('review');
+  reviews.innerText = reviews.value; 
+  
+}
+
+getReviews = () => {
+  var reviews = document.getElementById('reviews');
+  var storedReview = localStorage.getItem('review');
+  reviews.innerText = storedReview.reviews; 
+}
