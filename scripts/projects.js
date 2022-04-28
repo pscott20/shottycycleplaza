cc64196f4bc124a748f43861be60176b51180b56
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-04-28 11:02:58 -0400

    added images for projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -55,3 +55,61 @@ $(document).ready(function()
     $("li:first-child a").focus();
 
 }); //end ready
+
+$(document).ready(function()
+{
+    // preload the image for each link
+    $("#future_project_trail_70 a").each(function()
+    {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr('href');
+    });
+
+    // set up the event handlers for each link
+    $("#future_project_trail_70 a").click(function(evt)
+    {
+        //swap image
+        var imageURL = $(this).attr("href");
+        $("#future_project_trail_70_main_image").attr("src", imageURL);
+
+        //swap caption
+        var caption = $(this).attr("title");
+        $("#future_project_trail_70_caption").text(caption);
+
+        //cancel the default action of the link
+        evt.preventDefault(); //jQuery cross-browser method
+    }); //end click
+
+    //move focus to first thumbnail
+    $("li:first-child a").focus();
+
+}); //end ready
+
+$(document).ready(function()
+{
+    // preload the image for each link
+    $("#future_project_fz_07 a").each(function()
+    {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr('href');
+    });
+
+    // set up the event handlers for each link
+    $("#future_project_fz_07 a").click(function(evt)
+    {
+        //swap image
+        var imageURL = $(this).attr("href");
+        $("#future_project_fz_07_main_image").attr("src", imageURL);
+
+        //swap caption
+        var caption = $(this).attr("title");
+        $("#future_project_fz_07_caption").text(caption);
+
+        //cancel the default action of the link
+        evt.preventDefault(); //jQuery cross-browser method
+    }); //end click
+
+    //move focus to first thumbnail
+    $("li:first-child a").focus();
+
+}); //end ready
