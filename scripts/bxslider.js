0559bcbf97b4b8883bbf035738630a5ee072e861
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-05-04 20:37:51 -0400

    updates

---
+++ scripts/bxslider.js
@@ -0,0 +1,19 @@
+//bxSlider used on Store page
+$(document).ready(function(){
+    $('#slider1').bxSlider(
+      caption = true,
+      maxSlide = 1
+    );
+  });
+  $(document).ready(function(){
+    $('#slider2').bxSlider( 
+      caption = true,
+      maxSlide = 1
+    );
+  });
+  $(document).ready(function(){
+    $('#slider3').bxSlider(
+      caption = true,
+      maxSlide = 1
+    );
+  });
--- scripts/default_scripts.js
+++ scripts/default_scripts.js
@@ -14,27 +14,6 @@ const reviewsArray = {
 
 const reviews = JSON.parse(localStorage.getItem('reviewArray')) || []; //Converts the JSON string into an object or returns an empty array
 
-
-//bxSlider used on Store page
-//$(document).ready(function(){
-//    $('.bxslider').bxSlider();
- // });
-
-//jQuery call to the accordion() method.
-//$(document).ready(function() {
-//  $("#projects-container").accordion({
-//      collapsible: true, 
-//      heightStyle: "content"
-//  });
-//});
-
-//JQuery call to the Tooltip() method
-//$(document).ready(function() {
-//  $(".review-form").tooltip({
-//  });
-//});
-
-
 //Customers Page
 
 //Only activate button once both forms have entries. Only allow the user to submit one entry
